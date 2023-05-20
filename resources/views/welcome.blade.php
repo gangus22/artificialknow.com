60ae6a47145cd322f32cfcfaf7ab3115cc295288
gangus22 <balogkristof13@gmail.com>
2023-05-20 20:24:19 +0200

    added example model, query and noindex

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        @php
+            //TODO: remove robots noindex when production ready
+        @endphp
+        <meta name="robots" content="noindex">
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>ArtificialKnow</title>
@@ -14,6 +19,14 @@
                 I'm nice and green, because TailwindCSS works now.
             </div>
             <div id="react-root"></div>
+            <div class="rounded-lg border border-slate-700 bg-amber-400 p-4">
+                @php
+                    use \App\Models\Example;
+                    /** @var Example $exampleModel */
+                    $exampleModel = Example::all()->first();
+                    echo $exampleModel->test_data;
+                @endphp
+            </div>
         </div>
     </body>
 </html>
