b6220a02480ebe16199274e173dbe7e5eb66a4f0
gangus22 <balogkristof13@gmail.com>
2023-08-29 20:47:37 +0200

    refine export script again (password + time in filename) and add apply script

---
+++ scripts/apply_snapshot.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+source ../.env
+
+env=$APP_ENV
+host=$DB_HOST
+port=$DB_PORT
+user=$DB_USERNAME
+pw=$DB_PASSWORD
+dbname=$DB_DATABASE
+
+mysql --user=$user --password=${DB_PASSWORD} -e "DROP DATABASE IF EXISTS ``$dbname``" &&
+mysql --user=$user --password=${DB_PASSWORD} -e "CREATE DATABASE ``$dbname``" &&
+mysql --user=$user --password=${DB_PASSWORD} $dbname < $1
--- scripts/export_database.sh
+++ scripts/export_database.sh
@@ -9,4 +9,4 @@ user=$DB_USERNAME
 pw=$DB_PASSWORD
 dbname=$DB_DATABASE
 
-mysqldump -u ${DB_USERNAME} -p --databases ${DB_DATABASE} > ../database/snapshots/snapshot-`date +"%Y-%m-%d"`.sql
+mysqldump -u ${DB_USERNAME} --password=${DB_PASSWORD} --databases ${DB_DATABASE} > ../database/snapshots/snapshot-`date +"%Y-%m-%d-%T"`.sql
