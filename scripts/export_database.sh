8b25aafa3855dff44f24f9fe634bebc6141cede0
gangus22 <balogkristof13@gmail.com>
2023-08-29 20:34:34 +0200

    refine export script

--- scripts/export_database.sh
+++ scripts/export_database.sh
@@ -6,6 +6,7 @@ env=$APP_ENV
 host=$DB_HOST
 port=$DB_PORT
 user=$DB_USERNAME
+pw=$DB_PASSWORD
 dbname=$DB_DATABASE
 
-
+mysqldump -u ${DB_USERNAME} -p --databases ${DB_DATABASE} > ../database/snapshots/snapshot-`date +"%Y-%m-%d"`.sql
