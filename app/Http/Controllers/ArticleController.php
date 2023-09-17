0a0054e606aa6d578e155602741fe9bf5bfb38d7
gangus22 <balogkristof13@gmail.com>
2023-09-17 18:07:16 +0200

    added todo to controller

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,6 +9,7 @@ class ArticleController extends Controller
 {
     public function __invoke(Page $page): JsonResponse
     {
+        // TODO: show actual article page
         return response()->json(json_decode($page->content->article), options: JSON_PRETTY_PRINT);
     }
 }
