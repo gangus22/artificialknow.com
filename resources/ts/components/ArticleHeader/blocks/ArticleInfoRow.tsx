3d3d9297550f0863f2c8018c11e8331409125a06
gangus22 <balogkristof13@gmail.com>
2023-10-06 22:03:27 +0200

    info row cleanup

--- resources/ts/components/ArticleHeader/blocks/ArticleInfoRow.tsx
+++ resources/ts/components/ArticleHeader/blocks/ArticleInfoRow.tsx
@@ -2,14 +2,10 @@ import { CalendarIcon, UserCircleIcon } from "@heroicons/react/24/outline";
 import React from "react";
 
 export const ArticleInfoRow: React.FC = () => (
-    <div className="flex gap-x-4">
-        <span className="flex items-center gap-x-1 text-sm font-bold text-slate-600">
-            <UserCircleIcon className="h-8 w-8 stroke-slate-600" />
-            Author
-        </span>
-        <span className="flex items-center gap-x-1 text-sm font-bold text-slate-600">
-            <CalendarIcon className="h-8 w-8 stroke-slate-600" />
-            Date
-        </span>
+    <div className="flex items-center gap-x-2 text-sm font-bold text-slate-600">
+        <UserCircleIcon className="h-8 w-8 stroke-slate-600" />
+        Author
+        <CalendarIcon className="h-8 w-8 stroke-slate-600" />
+        Date
     </div>
 );
