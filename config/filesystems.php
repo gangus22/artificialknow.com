a0b86d14ef04279c76d1ed9f05fe4b4e79479a06
gangus22 <balogkristof13@gmail.com>
2023-10-14 22:06:03 +0200

    add images upload folder path

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,14 @@ return [
             'throw' => false,
         ],
 
+        'storage' => [
+            'driver' => 'local',
+            'root' => storage_path('images'),
+            'url' => env('APP_URL').'/images',
+            'visibility' => 'private',
+            'throw' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
