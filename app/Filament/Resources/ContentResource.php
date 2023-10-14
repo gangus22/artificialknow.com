7942076576772658c7077b36236f6ab82cff3717
gangus22 <balogkristof13@gmail.com>
2023-10-14 22:53:42 +0200

    added content resource

---
+++ app/Filament/Resources/ContentResource.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContentResource\Pages;
+use App\Models\Content;
+use App\Models\Page;
+use Filament\Forms\Components\Builder;
+use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ContentResource extends Resource
+{
+    protected static ?string $model = Content::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('page_id')
+                    ->required()
+                    ->getOptionLabelFromRecordUsing(fn(Page $page) => $page->id.' - '.$page->name)
+                    ->relationship('page')
+                    ->columnSpan(2),
+                TextInput::make('name')
+                    ->columnSpan(2)
+                    ->required(),
+                Builder::make('article')
+                    ->columnSpan(2)
+                    ->blocks([
+                        Block::make('heading')
+                            ->schema([
+                                TextInput::make('text')
+                                    ->label('content')
+                                    ->required(),
+                                Select::make('level')
+                                    ->options([
+                                        '2' => 'h2',
+                                        '3' => 'h3',
+                                        '4' => 'h4',
+                                    ])
+                                    ->required(),
+                            ]),
+                        Block::make('paragraph')
+                            ->schema([
+                                RichEditor::make('content')
+                                    ->disableToolbarButtons([
+                                        'h1',
+                                        'h2',
+                                        'h3',
+                                        'attachFiles'
+                                    ])
+                                    ->label('Paragraph')
+                                    ->required(),
+                            ]),
+                        Block::make('image')
+                            ->schema([
+                                FileUpload::make('url')
+                                    ->label('Image')
+                                    ->image()
+                                    ->required()
+                                    ->disk('storage'),
+                                TextInput::make('alt')
+                                    ->label('Alt text')
+                                    ->required(),
+                            ]),
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id'),
+                TextColumn::make('name')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContents::route('/'),
+            'create' => Pages\CreateContent::route('/create'),
+            'edit' => Pages\EditContent::route('/{record}/edit'),
+        ];
+    }
+}
