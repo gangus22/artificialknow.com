a37789dccc8638921a8e75bc7611d8572e0b58e5
gangus22 <balogkristof13@gmail.com>
2023-10-15 21:22:17 +0200

    replace H1 component

--- resources/ts/components/Heading/blocks/Heading.tsx
+++ resources/ts/components/Heading/blocks/Heading.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classnames";
 
 export const Heading: React.FC<{ text: string; level: number }> = ({
     text,
@@ -7,7 +8,15 @@ export const Heading: React.FC<{ text: string; level: number }> = ({
     const HeadingTag = `h${level}` as keyof React.JSX.IntrinsicElements;
 
     return (
-        <HeadingTag className="font-sans text-5xl font-black">
+        <HeadingTag
+            className={classNames(
+                "font-sans font-black",
+                level === 1 && "text-5xl",
+                level === 2 && "text-3xl",
+                level === 3 && "text-2xl",
+                level === 4 && "text-xl"
+            )}
+        >
             {text}
         </HeadingTag>
     );
