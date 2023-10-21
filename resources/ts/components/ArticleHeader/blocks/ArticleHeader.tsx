e4d2f4643cf2165ee4f6a45cb02809b8f4eb561a
gangus22 <balogkristof13@gmail.com>
2023-10-21 22:49:01 +0200

    smaller tsx fixes

--- resources/ts/components/ArticleHeader/blocks/ArticleHeader.tsx
+++ resources/ts/components/ArticleHeader/blocks/ArticleHeader.tsx
@@ -3,7 +3,7 @@ import { ArticleInfoRow } from "./ArticleInfoRow";
 import { Heading } from "../../Heading/blocks/Heading";
 
 export const ArticleHeader: React.FC<{ text: string }> = ({ text }) => (
-    <div className="mt-4 mb-8 flex flex-col gap-y-4 border-b-2 border-dotted border-slate-300 pb-8">
+    <div className="mb-8 flex flex-col gap-y-4 border-b-2 border-dotted border-slate-300 pb-8">
         <Heading level={1} text={text} />
         <ArticleInfoRow />
     </div>
--- resources/ts/components/Breadcrumbs/blocks/Breadcrumbs.tsx
+++ resources/ts/components/Breadcrumbs/blocks/Breadcrumbs.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+// TODO: clickable breadcrumbs
+
 export const Breadcrumbs: React.FC<{ url: string }> = ({ url }) => (
     <div className="w-max rounded-full px-4 py-2 text-sm bg-secondary-100">
-        {url.replace("/", " / ")}
+        {url.replaceAll("/", " / ")}
     </div>
 );
