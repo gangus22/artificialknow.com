929ff3a50799e351c07729c7db349fe25634e624
gangus22 <balogkristof13@gmail.com>
2023-11-12 13:51:37 +0100

    add redirect resource

---
+++ app/Filament/Resources/RedirectResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RedirectResource\Pages;
+use App\Models\Page;
+use App\Models\Redirect;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class RedirectResource extends Resource
+{
+    protected static ?string $model = Redirect::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-arrows-right-left';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('from')
+                    ->required()
+                    ->options(Page::query()->pluck('url')),
+                TextInput::make('to')
+                    ->url()
+                    ->required(),
+                Select::make('type')
+                    ->required()
+                    ->options([
+                        302 => 'Temporary (302)',
+                        301 => 'Permanent (301)',
+                    ])
+                    ->columnSpan(2)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRedirects::route('/'),
+            'create' => Pages\CreateRedirect::route('/create'),
+            'edit' => Pages\EditRedirect::route('/{record}/edit'),
+        ];
+    }
+}
