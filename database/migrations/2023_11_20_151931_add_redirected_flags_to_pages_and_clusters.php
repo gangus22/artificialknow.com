e54fc93ecf1ea693de17d653424b5a3f87e0d56b
gangus22 <balogkristof13@gmail.com>
2023-11-20 17:32:33 +0100

    add redirected flag to pages and clusters, and remove unique index

---
+++ database/migrations/2023_11_20_151931_add_redirected_flags_to_pages_and_clusters.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->dropIndex('pages_name_unique');
+
+            $table->boolean('redirected')->default(false);
+        });
+
+        Schema::table('clusters', function (Blueprint $table) {
+            $table->boolean('redirected')->default(false);
+        });
+    }
+};
