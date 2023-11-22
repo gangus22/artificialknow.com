f7649dcf7fe31896758ef4938f2e52288c86a74a
gangus22 <balogkristof13@gmail.com>
2023-11-22 18:09:04 +0100

    add pageFactory with Cluster redirect tests

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Traits\HasCachedUrls;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -28,6 +29,7 @@ use Illuminate\Support\Carbon;
  */
 class Page extends Model
 {
+    use HasFactory;
     use HasCachedUrls;
 
     protected $casts = [
