ba31e47726af0cd73950f13dfe6e64291434b1af
gangus22 <balogkristof13@gmail.com>
2023-11-23 19:00:41 +0100

    login page removal leftover

--- resources/ts/Inertia/LoginPage.tsx
+++
@@ -1,9 +0,0 @@
-import React, { memo } from "react";
-import { LoginForm } from "../components/LoginForm/blocks/LoginForm";
-
-export const LoginPage: React.FC = () => (
-    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-bl from-slate-800 from-50% to-secondary-900">
-        <LoginForm />
-    </div>
-);
-export default memo(LoginPage);
