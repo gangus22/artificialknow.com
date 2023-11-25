61ea3efcffe4492a58b139f0889cf6e3777e7ae5
gangus22 <balogkristof13@gmail.com>
2023-11-25 17:02:45 +0100

    add article schema markup

--- resources/ts/models/Page.ts
+++ resources/ts/models/Page.ts
@@ -5,7 +5,17 @@ export type Page = {
     id: number;
     path: string;
     url: string;
+    meta: MetaData;
     title_tag: string;
     cluster: Cluster;
     content: Content;
+    created_at: string;
+    updated_at: string;
+};
+
+type MetaData = {
+    metaDescription: string;
+    "og:title": string;
+    "og:description": string;
+    "og:image": string;
 };
