919d5510849241a7297401d7520a6d33f0e3ccbf
gangus22 <balogkristof13@gmail.com>
2023-12-01 22:56:13 +0100

    add ToC component

--- resources/ts/components/Heading/blocks/Heading.tsx
+++ resources/ts/components/Heading/blocks/Heading.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import classNames from "classnames";
 
-export const Heading: React.FC<{ text: string; level: number }> = ({
-    text,
-    level,
-}) => {
+export const Heading: React.FC<{
+    text: string;
+    level: number;
+    id?: string;
+}> = ({ text, level, id }) => {
     const HeadingTag = `h${level}` as keyof React.JSX.IntrinsicElements;
 
     return (
         <HeadingTag
+            id={id}
             className={classNames(
                 "font-sans font-black",
-                level === 2 && "text-2xl md:text-3xl my-8",
-                level === 3 && "text-xl md:text-2xl my-4",
-                level === 4 && "text-lg md:text-xl my-2"
+                level === 2 && "text-2xl md:text-3xl mb-4 md:mb-8",
+                level === 3 && "text-xl md:text-2xl mb-2 md:mb-4",
+                level === 4 && "text-lg md:text-xl mb-1 md:mb-2"
             )}
         >
             {text}
--- resources/ts/components/PageFooter/blocks/PageFooter.tsx
+++ resources/ts/components/PageFooter/blocks/PageFooter.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const PageFooter: React.FC = () => (
-    <div className="sticky top-full bottom-0 flex h-12 w-screen items-center justify-between bg-slate-800 px-4 text-slate-300">
+    <div className="sticky top-full bottom-0 flex h-12 w-full items-center justify-between bg-slate-800 px-4 text-slate-300">
         <div>ArtificialKnow, 2023</div>
         <a className="underline" href="/about-us">
             About Us
