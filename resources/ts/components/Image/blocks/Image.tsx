f59090511885ed317326536cfbe3f742a6dd6f92
gangus22 <balogkristof13@gmail.com>
2023-12-06 00:46:46 +0100

    storage img path fix

--- resources/ts/components/Image/blocks/Image.tsx
+++ resources/ts/components/Image/blocks/Image.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Image: React.FC<{ url: string; alt: string }> = ({ url, alt }) => (
-    <img className="my-4" alt={alt} src={`/${url}`} />
+    <img className="my-4" alt={alt} src={`/storage/${url}`} />
 );
