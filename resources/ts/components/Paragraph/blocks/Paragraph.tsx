75051a6ce034f266a179c9a36ef93846ca82c4a0
gangus22 <balogkristof13@gmail.com>
2023-12-06 12:42:15 +0100

    paragraph formatting fix

--- resources/ts/components/Paragraph/blocks/Paragraph.tsx
+++ resources/ts/components/Paragraph/blocks/Paragraph.tsx
@@ -4,7 +4,7 @@ export const Paragraph: React.FC<{ htmlContent: string }> = ({
     htmlContent,
 }) => (
     <div
-        className="leading-loose"
+        className="leading-loose [&>blockquote]:ps-2 [&>p>a]:text-secondary-400 [&>p>a]:hover:text-secondary-600 [&>ol]:list-inside [&>ul]:list-inside [&>ul]:list-disc [&>ol]:list-decimal [&>blockquote]:border-l-2 [&>blockquote]:border-slate-200 [&>blockquote]:text-slate-600 [&>p>a]:hover:underline"
         // eslint-disable-next-line react/no-danger
         dangerouslySetInnerHTML={{ __html: htmlContent }}
     />
